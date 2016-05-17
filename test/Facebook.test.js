bd8fc77e8a86e160cca1a16786a9cf0eedb360da
Charles Bido <charlesbido@me.com>
2016-05-17 00:56:54 -0500

    init tests

---
+++ test/Facebook.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let should = require('chai').should();
+
+describe('Facebook', () => {
+  describe('#post()', () => {
+    it("should generate a post and send it to the Robotics Club's Facebook group");
+  });
+  describe("#event()", () => {
+    it("should generate an event on the Robotics Club's Facebook group");
+  });
+});
