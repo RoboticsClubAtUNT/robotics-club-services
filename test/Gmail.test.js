d9d14c275ebf14a885e824c5da36eb5ff6662030
Charles Bido <charlesbido@me.com>
2016-05-17 01:00:57 -0500

    init test

---
+++ test/Gmail.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let should = require('chai').should();
+
+describe('Gmail', () => {
+  describe('#sendToOne()', () => {
+    it("it send an email to a single reciepient");
+  });
+  describe("#sendToMany()", () => {
+    it("it send an email to a many reciepient");
+  });
+});
